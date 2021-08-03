ed363e9611b62cc82c9d0deccd919f1c8328217f
Shweta Gautam <shweta.gautam@nagarro.com>
2021-08-03 10:11:33 +0530

    correction

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
-
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-app.use(requireHTTPS);
-app.use(express.static('./dist/synonyms-project'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/synonymsProject'));
 
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: './dist/synonyms-project/'}
-  );
-  });
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/synonymsProject/index.html'));
+});
 
-app.listen(process.env.PORT || 3000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
